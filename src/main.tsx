8591d8489337c57880741c210ba4a5020d50d3b8
mohammedjahangir0970-art <mohammedjahangir0970@gmail.com>
2026-06-28 15:10:26 +0100

    trigger build: update comment to force redeploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// CabinClear UK Store - Deployed via Cloudflare Pages
+// Trigger deployment: refresh token
+// This comment ensures the build triggers
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
